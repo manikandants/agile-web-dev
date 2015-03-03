32c2a3a2fb8080df54e6fe5fd07144d0ba1dfba8
Manikandan T S <manikandan.citeee@gmail.com>
2015-03-03 15:48:30 +0530

    Scenario 1 complete

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,14 @@
 bddApp.service('calculatorService', function(){
   var add = function(operand1, operand2) {
-    return operand1 + operand2;
+	if((operand1 != undefined)&&(operand2 != undefined)){
+		if(parseInt(operand1) && parseInt(operand2)){
+			return parseInt(operand1) + parseInt(operand2);
+		}else{
+			return 0;
+		}
+	}else{
+		return 0;
+	}
   };
   return {
     add: add
