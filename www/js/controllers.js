fd35a2016848decc9ce618cc4031a76509cb24e3
Manikandan T S <manikandan.citeee@gmail.com>
2015-03-11 09:45:46 +0530

    Added Travis CI

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,9 @@
 bddApp.controller('homeController',['$scope', '$http', '$location', 'calculatorService', function($scope, $http, $location, calculatorService){
+	$scope.operand1 = 10;
+	$scope.operator = '+';
+	$scope.operand2 = 15;
 	$scope.add = function(){
 		$scope.result = calculatorService.add($scope.operand1, $scope.operand2);
 	};
+	$scope.add();
 }]);
